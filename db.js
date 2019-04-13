2e4df9df44c96e0500b45977feba7a087b90edeb
GURINDER2001 <49118264+GURINDER2001@users.noreply.github.com>
2019-04-13 16:57:21 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,55 +1,49 @@
-const Sequelize=require('sequelize')
+const Sequelize = require('sequelize')
 
-const db= new Sequelize('cartdb','cartuser','cartpass',{
-    host:'localhost',
-    dialect:'sqlite',
-    pool:{
-        min:0,
-        max:5,
+const db = new Sequelize('shopdb', 'shopper', 'shoppass', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    storage: __dirname + '/shopdb.db',
+    pool: {
+        min: 0,
+        max: 5,
     }
 })
 
-const User= db.define('users',{
-    id:{
+const User = db.define('users', {
+    id: {
         type: Sequelize.INTEGER,
-        autoIncrement:true,
+        autoIncrement: true,
         primaryKey: true
     },
-    name:{
+    name: {
         type: Sequelize.STRING,
-        allowNull: false
-    }
-    
-})
-
-const product=db.define('products',{
-    id:{
-        type: Sequelize.INTEGER,
-        autoIncrement:true,
-        primaryKey: true
-    },
-    name:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    manufacturer:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    price:{
-        type: Sequelize.INTEGER,
         allowNull: false,
-        defaultValue:0
+    }
+})
+
+const Product = db.define('products', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    manufacturer: Sequelize.STRING,
+    price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0.0
     }
 })
 
 db.sync()
-.then(()=>{
-    console.log("database is synced");
-    }).catch((err )=>{
-        console.log("error creating database");
-        })
+    .then(() => console.log("Database has been synced"))
+    .catch((err) => console.error("Error creating database"))
 
-exports=module.exports={
-    User, product
+exports = module.exports = {
+    User, Product
 }
