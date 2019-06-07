3e6e4f09cb091f57ae3ee5f58a9cfa64393508e2
GURINDER2001 <49118264+GURINDER2001@users.noreply.github.com>
2019-06-07 14:52:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path = require('path')
 
 const app = express();
-
+const SERVER_PORT= process.env.PORT ||2567
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/api', require('./routes/api').route)
 
-app.listen(2567, () => console.log('Server started at http://localhost:2567'))
+app.listen(SERVER_PORT, () => console.log('Server started at http://localhost:2567'))
